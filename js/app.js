0c3a8f9fdd30b7cf0ae5ec5ef2fd2dc00da1ffa0
pjlunkes <160961843+pjlunkes@users.noreply.github.com>
2024-06-18 23:53:08 -0300

    icone play
    vc pediu, vc receberas

--- js/app.js
+++ js/app.js
@@ -1,2 +1,24 @@
 //Utilizar JS para fazer o tamanho do paralelogramo dinamico
 
+document.getElementById('icon1').addEventListener('click', function (evitarEvento) {
+    evitarEvento.preventDefault()
+    document.getElementById('icon1').style.display = 'none';
+    document.getElementById('icon2').style.display = 'inline';
+});
+
+document.getElementById('icon2').addEventListener('click', function (evitarEvento) {
+    evitarEvento.preventDefault()
+    document.getElementById('icon2').style.display = 'none';
+    document.getElementById('icon1').style.display = 'inline';
+});
+let btnProf = document.getElementById('btnLogin').addEventListener('click', function(){
+    window.location.href = "profile.html"
+
+});
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
