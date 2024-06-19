e8da3a2a54fd7827ffcdcfe8e5367065b26c2b2d
Diego <89855790+Lambaris@users.noreply.github.com>
2024-06-19 15:42:48 -0300

    Footery tootery

--- js/app.js
+++ js/app.js
@@ -23,7 +23,4 @@ myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus()
 })
 
-let nome = document.getElementById("nome")
-let senha = document.getElementById("senha")
-let nomeR = document.getElementById("nome")
-let senhaR = document.getElementById("senha")
+// 1 == 1? console.log("True"):console.log("False");
